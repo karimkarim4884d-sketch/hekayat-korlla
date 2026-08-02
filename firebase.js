48bd7c24c1ad4b6ec1fa8c76455347be2a3a1c86
karimkarim4884d-sketch <karimkarim4884d@gmail.com>
2026-08-02 08:13:13 +0300

    Initialize Firebase app and database connection

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
-// سيتم إضافة إعدادات Firebase هنا لاحقًا
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfAqlHPGsdZ0c0Ndwrl2YfSuu2Z48FjDI",
+  authDomain: "zack-cinema.firebaseapp.com",
+  databaseURL: "https://zack-cinema-default-rtdb.firebaseio.com",
+  projectId: "zack-cinema",
+  storageBucket: "zack-cinema.firebasestorage.app",
+  messagingSenderId: "166273951321",
+  appId: "1:166273951321:web:1472ff55831ade6b087348"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
